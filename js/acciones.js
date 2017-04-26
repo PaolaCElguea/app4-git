b8253195191824cb860f13bf1cb46913bb85a7b5
PaolaCElguea <elguea61@gmail.com>
2017-04-26 08:05:34 -0500

    7s

--- js/acciones.js
+++ js/acciones.js
@@ -8,24 +8,24 @@ $(document).ready(function(e){
 		$('#disp table td').eq(9).text(device.version);
 		$('#disp table td').eq(11).text(device.uuid);
 	document.addEventListener("pause",function(){
-		escribehistoria('la app de pausó');
+		escribehistoria('la app se pausó');
 	},false);//pause
 	document.addEventListener("resume",function(){
-		escribehistoria('la app se reinicio');
+		escribehistoria('la app se reinició');
 	},false);//resume
 	document.addEventListener("online",function(){
-	    escribehistoria('la app se conecto a la red');
+		escribehistoria('la app se conecto a la red');
 	},false);//se conecto
 	document.addEventListener("offline",function(){
-	    escribehistoria('la app de desconecto de la red');
+		escribehistoria('la app se desconecto de la red');
 	},false);//se desconecto
-	
+		
+			
 	},false);//ready device
+	
 	
 });//document
 
 function escribehistoria(accion){
 	$('#eHistoria').append('<li>'+accion+'</li>');
 }
-	
-	
